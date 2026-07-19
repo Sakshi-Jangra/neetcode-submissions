ecc38e46e7b640397ca15f7f4d053466869c98b5
SAKSHI <49511150+Sakshi-Jangra@users.noreply.github.com>
2026-07-19 18:52:36 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0, end = s.length()-1;
+        
+        while(start < end)
+        {
+            while(start < end && !isalnum(s[start]))
+                start++;
+            
+            while(start < end && !isalnum(s[end]))
+                end--;
+            
+            if(start < end && tolower(s[start++]) != tolower(s[end--]) )
+                return false;
+        }
+        return true;            
+    }
+};
