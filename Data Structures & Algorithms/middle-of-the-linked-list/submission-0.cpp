7856af16f344a4f085a220a2aa8b0daa4d4ceeb0
SAKSHI <49511150+Sakshi-Jangra@users.noreply.github.com>
2026-07-31 01:54:01 +0530

    Add: middle-of-the-linked-list - submission-0

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-0.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *curr = head;
+        int count = 0;
+        while(curr != NULL){
+            curr = curr->next;
+            count++;
+        }
+        count = (count / 2);
+        curr = head;
+
+        while(count--){
+            curr = curr->next;
+        }
+        return curr;
+    }
+};
