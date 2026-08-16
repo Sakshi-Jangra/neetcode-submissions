3eeffb2635b8c1a3d24eca90e0f85a35f7e3ab12
SAKSHI <49511150+Sakshi-Jangra@users.noreply.github.com>
2026-08-16 20:29:10 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+public:
+    stack<long long int> s;
+    long long int minVal = INT_MAX;
+    MinStack() {
+         
+    }
+    
+    void push(int val) {    
+        if(s.empty()){
+            minVal = val;
+        }else if(val < minVal){
+            s.push((long long)2*val - minVal);
+            minVal = val;
+            return;
+        }   
+        s.push(val);
+    }
+    
+    void pop() {
+        if(s.top() <= minVal){
+            minVal = 2*minVal - s.top();
+        }
+        s.pop(); 
+    }
+    
+    int top() {
+        if(s.top() <= minVal){
+            return minVal;
+        }
+        return s.top();
+    }
+    
+    int getMin() {
+        return minVal;
+    }
+};
